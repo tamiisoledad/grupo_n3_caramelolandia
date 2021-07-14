1d50047c471ae46fa1dd9ef11770017e48d9136d
chinosama <85371007+chinosama@users.noreply.github.com>
2021-07-14 11:05:18 -0300

    avanza app.js

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const path = require(path);
 const port = 3030;
 app.use(express.estatic('public'));
 
-app.get('/detalleprod',(req,res) => res.sendFile(path.join(__dirname,'views','detalleprod.html')))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/detailprod',(req,res) => res.sendFile(path.join(__dirname,'views','detailprod.html')))
+app.get('/cart',(req,res) => res.sendFile(path.join(__dirname,'views','cart.html')))
 
 
 app.listen(port,() => console.log(`Server running in http://localhost:${port}`))
