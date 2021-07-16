96e6f038aa5b75af54b56e7afa1c1a39cbf296a7
MatiDragon-YT <clarosisimatias@gmail.com>
2021-07-15 23:07:10 -0300

    optimize nuestro flujo de trabajo
    Para agregar nuevas paginas estaticas, solo modifican el arreglo constante *page* y listo ;)

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = 3030;
+const
+	express = require('express'),
+	app = express(),
+	path = require('path'),
+	port = 3030;
 app.use(express.static('public'));
 
-app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','index.html')))
-app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
-app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
-app.get('/contact',(req,res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
-app.get('/productDetail',(req,res) => res.sendFile(path.join(__dirname,'views','productDetail.html')))
-app.get('/productCart',(req,res) => res.sendFile(path.join(__dirname,'views','productCart.html')))
+direction = (x) => (req,res) => res.sendFile(path.join(__dirname, 'views', x))
+app.get('/', direction('home.html'));
 
+const page = ['register','login','contact','productDetail','productCart'];
+for (let i = page.length - 1; i >= 0; i--) {
+	app.get(`/${page[i]}`, direction(`${page[i]}.html`));
+}
 
 app.listen(port,() => console.log(`Server running in http://localhost:${port}`))
