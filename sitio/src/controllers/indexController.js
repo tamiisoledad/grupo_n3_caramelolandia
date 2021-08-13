d08fd60b305f9af36638a84fb9e32e3a20517d0f
tamiisoledad <tamiisoledad4@gmail.com>
2021-08-13 20:54:16 -0300

    vista contacto renderizada

--- sitio/src/controllers/indexController.js
+++ sitio/src/controllers/indexController.js
@@ -1,3 +1,4 @@
 module.exports = {
-    index : (req,res) => res.render("index", {title: "Caramelolandia"})
+    index : (req,res) => res.render("index", {title: "Caramelolandia"}),
+    contacto : (req,res) => res.render("contact",{title:"Contacto"}) 
 }
