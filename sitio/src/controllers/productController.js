0035b9bf1b73a0ac4a302753db10597ee51768a3
chinosama <85371007+chinosama@users.noreply.github.com>
2021-08-13 20:52:23 -0300

    corrige rutas

--- sitio/src/controllers/productController.js
+++ sitio/src/controllers/productController.js
@@ -1,6 +1,9 @@
 module.exports = {
-    productsDetail : (req,res) => res.render("productDetail",{title:"Detalle de producto"}),
-    productsCart : (req,res) => res.render("productCart", {title: "Carrito"}),
+    productDetail : (req,res) => res.render("productDetail",{title:"Detalle de producto"}),
+
+    productCart : (req,res) => res.render("productCart", {title: "Carrito"}),
+
     productAdd : (req,res)=> res.render("productAdd", {title: "Agregar producto"}),
+
     productEdit : (req,res)=> res.render("productEdit", {title: "Editar producto"})
 }
