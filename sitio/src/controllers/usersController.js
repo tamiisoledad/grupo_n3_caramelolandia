91c56695dafdb740d69ac3d0a050b8c231e2d251
MatiDragon-YT <clarosisimatias@gmail.com>
2021-09-01 21:18:15 -0300

    Enlaces actualizados

--- sitio/src/controllers/usersController.js
+++ sitio/src/controllers/usersController.js
@@ -1,4 +1,4 @@
 module.exports = {
     login : (req,res) => res.render("login",{title:"Iniciar sesión"}),
-    register : (req,res) => res.render("register",{title:"Registrarse"})
+    register : (req,res) => res.render("register")
 }
