56b7209e21da5710b09ac05718b859ae1b1861ed
tamiisoledad <tamiisoledad4@gmail.com>
2021-09-03 15:53:09 -0300

    index dinamico

--- sitio/src/controllers/indexController.js
+++ sitio/src/controllers/indexController.js
@@ -1,5 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+const productos = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/productos.json"), "utf-8"));
+
 module.exports = {
-    index : (req,res) => res.render("index"),
+    index : (req,res) => res.render("index",{
+        productos
+    }),
     admin : (req,res) => res.render("admin/admin"),
     contacto : (req,res) => res.render("contact") 
 }
