d5eaf1f3d5b929041c9f3834648483492f0be4d8
chinosama <85371007+chinosama@users.noreply.github.com>
2021-09-09 13:19:59 -0300

    avanza process register

--- sitio/src/controllers/usersController.js
+++ sitio/src/controllers/usersController.js
@@ -1,4 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+let productos = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','productos.json'),'utf-8'));
+let users = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','users.json'),'utf-8'));
+
+
 module.exports = {
     login : (req,res) => res.render("login"),
-    register : (req,res) => res.render("register")
+
+    register : (req,res) => res.render("register",{
+        productos
+    }),
+    
+    processRegister : (req,res) => {
+        const {name,email,password} = req.body;
+
+        let user = {
+            id : users[users.length-1] ? users[users.length-1].id + 1 : 1,
+            name : name.trim(),
+            email : email.trim(),
+            password : bcryptjs.hashSync(password, 10),
+            image : 'default-image.png',
+            rol : "user"
+        }
+
+        users.push(user);
+        fs.writeFileSync(path.join(__dirname,'..','data','usuarios.json'),JSON.stringify(users,null,2),'utf-8');
+        return res.redirect('/users/login')
+    }
 }
--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -1,8 +1,9 @@
 var express = require('express');
-const {login, register} = require("../controllers/usersController");
+const {login, register, processRegister} = require("../controllers/usersController");
 
 module.exports = express.Router()
 	// GET users listing.
 	.get('/login', login)
 	.get('/register', register)
+	.post('/register',processRegister)
 ;
