9d1214510d6c43425108caf9cc88f14e8ec4e12b
chinosama <85371007+chinosama@users.noreply.github.com>
2021-09-12 18:55:45 -0300

    register terminado

--- sitio/src/controllers/usersController.js
+++ sitio/src/controllers/usersController.js
@@ -9,33 +9,11 @@ let users = JSON.parse(fs.readFileSync(path.join(__dirname,'..','data','users.js
 
 module.exports = {
     login : (req,res) => res.render("login"),
-
-    register : (req,res) => res.render("register",{
-        productos
-    }),
-    
-    processRegister : (req,res) => {
-        return res.send(req.body)
-        const {name,email,password,image,terms,sales} = req.body;
-
-        let user = {
-            id : users[users.length-1] ? users[users.length-1].id + 1 : 1,
-            name : name.trim(),
-            email : email.trim(),
-            password : bcryptjs.hashSync(password, 10),
-            image : 'default-image.png',
-            rol : "user"
-        }
-
-        users.push(user);
-        fs.writeFileSync(path.join(__dirname,'..','data','users.json'),JSON.stringify(users,null,2),'utf-8');
-        return res.redirect('/users/login')
-    },
     processLogin : (req,res) => {
         let errors = validationResult(req)
         if(errors.isEmpty()){
             const {email} = req.body
-        let usuario =usuarios.find(usuario=>usuario.email === email)
+        let usuario = users.find(usuario=>usuario.email === email)
         req.session.userLogin={
             id: usuario.id,
             nombre: usuario.nombre,
@@ -45,7 +23,29 @@ module.exports = {
         }else{
             return res.render('login',{errores:errors.mapped()})
         }
-       
+    },
+
+    register : (req,res) => res.render("register",{
+        productos
+    }),
+    
+    processRegister : (req,res) => {
+        const {name,email,password,terms,sales} = req.body;
+
+        let user = {
+            id : users[users.length-1] ? users[users.length-1].id + 1 : 1,
+            name : name,
+            email : email,
+            password : bcryptjs.hashSync(password, 10),
+            image : req.file ? req.file.filename : 'default-image.png',
+            rol : "user",
+            terms : terms,
+            sales : sales
+        }
+
+        users.push(user);
+        fs.writeFileSync(path.join(__dirname,'..','data','users.json'),JSON.stringify(users,null,2),'utf-8');
+        return res.redirect('/users/login')
     },
     logout : (req,res)=> {
         req.session.destroy()
--- sitio/src/routes/users.js
+++ sitio/src/routes/users.js
@@ -1,12 +1,14 @@
 var express = require('express');
 const {login, register, processRegister, processLogin, logout} = require("../controllers/usersController");
 const loginValidator = require('../validations/loginValidation');
+const uploadFile = require('../middlewares/profileMulter')
+
 
 module.exports = express.Router()
 	// GET users listing.
 	.get('/login', login)
 	.get('/register', register)
-	.post('/register',processRegister)
-	.post('/login',loginValidator, processLogin)
+	.post('/register',uploadFile.single('image'),processRegister)
+	.post('/login',loginValidator,processLogin)
 	.get('/logout',logout)
 ;
