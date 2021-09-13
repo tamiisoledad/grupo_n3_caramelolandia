a0bc5972bc8a536f84b83e434d97a084c1d3cae0
MatiDragon-YT <clarosisimatias@gmail.com>
2021-09-13 17:29:22 -0300

    Panel Admin, abierto

--- sitio/src/routes/index.js
+++ sitio/src/routes/index.js
@@ -1,10 +1,11 @@
-var express = require('express');
+const express = require('express');
 const { index, admin, contacto, search} = require("../controllers/indexController")
+const adminUserCheck = require('../middlewares/adminUserCheck');
 
 module.exports = express.Router()
 	/* GET home page. */
 	.get('/', index)
-	.get('/admin',admin)
+	.get('/admin', adminUserCheck, admin)
 	.get('/contact', contacto)
 	.get('/search', search)
 ;
