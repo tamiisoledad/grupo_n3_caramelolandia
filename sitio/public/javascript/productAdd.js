0c855982dd016ab2802770fcc69c72f00c23f15e
tamiisoledad <tamiisoledad4@gmail.com>
2021-11-20 10:35:01 -0300

    preview image

---
+++ sitio/public/javascript/productAdd.js
@@ -0,0 +1,26 @@
+window.addEventListener("click", () => {
+    document.getElementById("imagen").onchange = function(e) {
+        // Creamos el objeto de la clase FileReader
+        let reader = new FileReader();
+      
+        // Leemos el archivo subido y se lo pasamos a nuestro fileReader
+        reader.readAsDataURL(e.target.files[0]);
+      
+        // Le decimos que cuando este listo ejecute el código interno
+        reader.onload = function(){
+          let preview = document.getElementById('preview'),
+                  image = document.createElement('img');
+      
+          image.src = reader.result;
+      
+          preview.innerHTML = '';
+          preview.append(image);
+        };
+      }
+
+      document.querySelector("#resetId").addEventListener("click", (e)=>{
+          e.preventDefault();
+          document.querySelector("#imagen").value ="";
+          preview.innerHTML = null
+      })
+})
